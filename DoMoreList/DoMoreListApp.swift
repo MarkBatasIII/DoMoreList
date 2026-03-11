31134f2cb23c040813f1e46e63596d184eba7055
MarkBatasIII <batas.marksalvadoriii@gmail.com>
2026-03-11 22:21:35 +0800

    Initial Commit

---
+++ DoMoreList/DoMoreListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoMoreListApp.swift
+//  DoMoreList
+//
+//  Created by Mark Batas on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct DoMoreListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
