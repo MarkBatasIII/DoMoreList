45f65a786ff94e10af444d1ddb7079e6b7e0f609
MarkBatasIII <batas.marksalvadoriii@gmail.com>
2026-03-11 22:45:28 +0800

    Created: ListView Content

--- DoMoreList/Views/ListView.swift
+++ DoMoreList/Views/ListView.swift
@@ -1,15 +1,33 @@
-//
-//  ListView.swift
-//  DoMoreList
-//
-//  Created by Mark Batas on 3/11/26.
-//
-
 import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                List {
+                    HStack {
+                        Image(systemName: "checkmark.circle")
+                            .foregroundColor(.green)
+                        Text("List")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                        Spacer()
+                    }
+                    .font(.title2)
+                    .padding(.vertical, 5)
+                }
+                .listStyle(PlainListStyle())
+            }
+            .navigationTitle("Things to-do")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Image(systemName: "plus")
+                }
+            }
+        }
     }
 }
 
