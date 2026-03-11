c7b8fbce52ed053cd25e61f2c5eb528d29cb1f9e
MarkBatasIII <batas.marksalvadoriii@gmail.com>
2026-03-11 22:44:17 +0800

    Created: ListView

---
+++ DoMoreList/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  DoMoreList
+//
+//  Created by Mark Batas on 3/11/26.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
